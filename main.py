2dbfc2e50676adc82d9e7860b0a3d1183a213e27
frog24male <frog24male@gmail.com>
2024-07-12 10:54:33 +0000

    Day 21 add description

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ col1 , col2=st.columns(2)
 
 with col1:
     st.image('images/photo.png')
+    
 with col2:
     st.title('Faizan Ali')
-    content="""my name is Faizan Ali. I am a highschool student i am studyinng in 12th class .i live in Pakistan."""
+    content="""My name is Faizan Ali. I am a Highschool student i am studying in 12th class .I live in Pakistan."""
     st.info(content)
+    
+st.write("Below you can find my projects. Feel free to contact me")
